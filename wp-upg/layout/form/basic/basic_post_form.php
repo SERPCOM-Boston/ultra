c4fcbfd9a3f47bccb5a78c5462c5d5a7a09ddd17
wilsonao <wilsonao@gmail.com>
2018-02-19 20:46:35 -0600

    Image Galleries

--- wp-upg/layout/form/basic/basic_post_form.php
+++ wp-upg/layout/form/basic/basic_post_form.php
@@ -10,43 +10,22 @@ function updateCatName(){
 	 }
 }
 </script>
-<div class="pure-form-wrapper">
 <form class="pure-form pure-form-stacked" method="post" enctype="multipart/form-data" action="">
 <fieldset>
-	
-		<div class="pure-control-group">
-    	    <?php
-			$put="";
-			ob_start ();
-				?>
-				<label for="file"><?php _e('Select Image', 'wp-upg'); ?></label>
-				<input class="pure-input-1" id="file" name="user-submitted-image[]" type="file" size="25" 
-				  <?php
-					if($options['image_required']=='1')
-					{
-						echo "required";
-					}
-					?>>
-				
-				<?php
-			$put=ob_get_clean (); 
-			echo apply_filters('upg_bulk_limit_submit_form',$put);
-    	
-			?>
-		</div>
-	
         <div class="pure-control-group">
             <label for="name">Photo Caption</label>
-            <input class="pure-input-1" id="name" name="user-submitted-title" type="text" value="" placeholder="Photo Caption">
+            <input class="pure-input-1 pure-input-rounded" id="name" name="user-submitted-title" type="text" value="" placeholder="Photo Caption" required>
         </div>
 
-
+           
+           
+		   
 		   <?php 
 		    if($options['primary_show_formshow_desp']=='1')
 			{
 				
-		   	if ($editor) 
-		   	{ ?>
+		   if ($editor) 
+		   { ?>
 				<div class="pure-controls">
 					<div class="usp_text-editor">
 			<?php $settings = array(
@@ -64,32 +43,34 @@ function updateCatName(){
 				    'drag_drop_upload' => true, // enable drag-drop
 				);
 				wp_editor('', 'upgcontent', apply_filters('upg_editor_settings', $settings)); ?>
-					</div>
+				
 				</div>
+			</div>
 			<?php 
 			} 
 			else 
 			{ 
 		?>
-			<div class="pure-control-group">
+		  <div class="pure-control-group">
 				 <label for="desp"><?php _e('Description', 'wp-upg'); ?></label>
 			
-				 <textarea class="pure-input-1" id="desp"  name="user-submitted-content" rows="5" placeholder="<?php _e('Post Content', 'wp-upg'); ?>" required></textarea>
-			</div>
+			<textarea class="pure-input-1 pure-input-rounded" id="desp"  name="user-submitted-content" rows="5" placeholder="<?php _e('Post Content', 'wp-upg'); ?>" required></textarea>
+			  </div>
 			<?php 
 			}
+
 				}
 			else
 			{
-				echo "<input type='hidden' name='user-submitted-content' value='No Information'> ";
+				echo "<input type='hidden' name='user-submitted-content' value=''> ";
 			}
-			?>
+						?>
 		   
 		   
       
 		<div class="pure-control-group">
             <label for="cat">Select Gallery</label>
-           <?php echo upg_droplist_category(); ?>
+           <?php echo str_replace('"cat"', '"gallery"', upg_droplist_category()); ?>
 		   </div>
 		   
 		   <div class="pure-control-group">
@@ -100,46 +81,101 @@ function updateCatName(){
 			$taxonomy = 'category';
             // Add filter to change the default taxonomy
             $taxonomy = apply_filters( 'wpmediacategory_taxonomy', $taxonomy );
+			//251 -- Photos Subcat
             if ( $taxonomy != 'category' ) {
                 $dropdown_options = array(
                     'taxonomy'        => $taxonomy,
                     'name'            => $taxonomy,
-                    'show_option_all' => __( 'View all categories', 'wp-media-library-categories' ),
+                    'show_option_all' => '',
                     'hide_empty'      => false,
                     'hierarchical'    => true,
                     'orderby'         => 'name',
+					'child_of'  	  => 251,
                     'show_count'      => true,
                     'walker'          => new wpmediacategory_walker_category_filter(),
-                    'value'           => 'slug'
+                    'value'           => 'slug',
+					 'echo'               => false,
                 );
             } else {
                 $dropdown_options = array(
                     'taxonomy'        => $taxonomy,
-                    'show_option_all' => __( 'View all categories', 'wp-media-library-categories' ),
+                    'show_option_all' => '',
                     'hide_empty'      => false,
                     'hierarchical'    => true,
                     'orderby'         => 'name',
+					'child_of'  	  => 251,
                     'show_count'      => false,
                     'walker'          => new wpmediacategory_walker_category_filter(),
-                    'value'           => 'id'
+                    'value'           => 'id',
+					 'echo'               => false,
                 );
             }
-            wp_dropdown_categories( $dropdown_options );
+          //  wp_dropdown_categories( $dropdown_options );
+			$cat_dropdown = wp_dropdown_categories( $dropdown_options );
+
+			$cat_dropdown = preg_replace( 
+					'^' . preg_quote( '<select ' ) . '^', 
+					'<select required ', 
+					$cat_dropdown
+				);
+
+			$cat_dropdown = preg_replace( '/<select (.*?) >/', '<select $1 size="5" multiple>', $cat_dropdown);
+			
+			echo  str_replace("name='cat'", "name='cat[]'", $cat_dropdown);
+			//wp_category_checklist(0,251);
+			
 			 ?>
         </div> 
 		
 		   
 
+		  
+       
 		<div class="pure-control-group">
-            <label for="tags">Tags</label>
+            <label for="tags">Tags</label><br />
 			<textarea name="tags" id="tags"></textarea>
-			<p class="howto" id="new-tag-post_tag-desc">Separate Tags with Commas</p>
+			<p class="howto" id="new-tag-post_tag-desc">Separate tags with commas</p>
         </div>
 		
+		
+		
+		<div class="pure-control-group">
+		
+            <?php
+			$put="";
+			ob_start ();
+				?>
+				<div id="images">
+				<label for="file_1"><?php _e('Select Image', 'wp-upg'); ?></label>
+				  <input class="pure-input-1-2 pure-input-rounded" id="file_1" name="user-submitted-image-1[]"  type="file" size="25" required>
+				  
+				</div>
+				<button type="button" onclick="addimages();">Add More Images</button>
+				<input type="hidden" id="image_count" name="image_count" value="1">
+				
+				<script>
+				
+				function addimages(){
+					var img_cnt = 1 + parseInt(document.getElementById("image_count").value);
+					var img_html = '<label for="file_' + img_cnt + '">Select Image </label><input class="pure-input-1-2 pure-input-rounded" id="file_' + img_cnt + '" name="user-submitted-image-' + img_cnt + '[]" type="file" size="25" >';
+					
+					document.getElementById('images').insertAdjacentHTML('beforeend', img_html);
+					document.getElementById("image_count").value = img_cnt;
+					
+				}
+				
+				</script>
+				<?php
+			$put=ob_get_clean (); 
+			echo apply_filters('upg_bulk_limit_submit_form',$put);
 
+			?>
+			
+		
+		</div>
 	
 		  <ul>
-		  <li><?php echo _e('Only image files are allowed.','wp-upg') ?></li>
+		  <li><?php echo _e('Only picture files are allowed.','wp-upg') ?></li>
 		  <li><?php echo _e('Maximum upload file size limit is','wp-upg') ?> <b><?php //echo ini_get('post_max_size'); ?> <?php echo size_format( wp_max_upload_size() ); ?></b></li>
 		  </ul>
         
@@ -184,10 +220,12 @@ function updateCatName(){
 		do_action( "upg_submit_form");
 		?>
 		
+		
 		 <div class="pure-controls">
 			<button type="submit" name="SN" class="pure-button pure-button-primary"><i class="fa fa-upload fa-lg"></i> <?php esc_html_e( 'Post', 'wp-upg' ); ?></button>
 			<?php wp_nonce_field('upg-nonce', 'upg-nonce', false); ?>
 		</div>
+		 </div>
 </fieldset>
 </form>	
-</div>	
+	
--- wp-upg/layout/form/post_image.php
+++ wp-upg/layout/form/post_image.php
@@ -26,20 +26,16 @@ else
 {
 		$author = ''; $url = ''; $email = ''; $tags = ''; $captcha = ''; $verify = ''; $content = ''; $category = ''; 
 	
-		$files = array();
-		if (isset($_FILES['user-submitted-image']))
-		{
-			$files = $_FILES['user-submitted-image'];
 		
-		}
 		
 		
 		
 		if (isset($_POST['user-submitted-content']))  $content  = upg_sanitize_content($_POST['user-submitted-content']);
 		if (isset($_POST['gallery'])) $gallery = $_POST['gallery'];
 		$post_category =  $_POST['cat'];
+		//print_r($_POST);
 		//check if cat exists
-		$cat_exists = term_exists($gallery, 'upg_cate' );	
+		$cat_exists = term_exists(intval($gallery), 'upg_cate' );	
 		global $suitecrm_link;
 		global $theuser;
 		$business_id = "";
@@ -49,13 +45,18 @@ else
 		
 
 		if($cat_exists) {
-			$term_info = get_term_by('slug',$gallery , 'upg_cate'); 
+			$term_info = get_term_by('id',intval($gallery) , 'upg_cate'); 
+			
 			//check if gallery name has been updated
-			if($term_info->name != $_POST['cat_name']){
+			/*if($term_info->name != $_POST['cat_name']){
 				wp_update_term($term_info->term_id, 'upg_cate', array('name'=>$_POST['cat_name']));
-			}
+			}*/
 			$gallery = $term_info->term_id;
 		}
+		else{
+			//print_r($_POST);
+		}
+			
 			
 		if(!$cat_exists && $business_id) { 
 			$result = wp_insert_term(
@@ -88,69 +89,80 @@ else
 		$content=str_replace("[","[[",$content);
 		$content=str_replace("]","]]",$content);
 		
-		$result = upg_submit($title, $files, $content, $gallery, $preview);
-		print_r($result);
-		$post_id = false; 
-		if (isset($result['id'])) $post_id = $result['id'];
-		
-		$error = false;
-		if (isset($result['error'])) $error = array_filter(array_unique($result['error']));
-		
-		 if ($post_id) 
-		{
-			//Submit extra fields data
-			for ($x = 1; $x <= 10; $x++) 
+		$image_cnt = $_POST['image_count'];
+		for($ic = 1; $ic<= $image_cnt;$ic++){
+			$files = array();
+			if (isset($_FILES['user-submitted-image-' . $ic]))
 			{
-				if (isset($_POST['upg_custom_field_'.$x]))
-				add_post_meta($post_id, 'upg_custom_field_'.$x, $_POST['upg_custom_field_'.$x]);
+				$files = $_FILES['user-submitted-image-' . $ic];
+			
+			}
+			$result = upg_submit($title, $files, $content, $gallery, $preview);
+			//print_r($result);
+			$post_id = false; 
+			if (isset($result['id'])) $post_id = $result['id'];
+			
+			$error = false;
+			if (isset($result['error'])) $error = array_filter(array_unique($result['error']));
+			
+			 if ($post_id) 
+			{
+				//Submit extra fields data
+				for ($x = 1; $x <= 10; $x++) 
+				{
+					if (isset($_POST['upg_custom_field_'.$x]))
+					add_post_meta($post_id, 'upg_custom_field_'.$x, $_POST['upg_custom_field_'.$x]);
+					
+				}
+				
+				//Ended to submit extra fields
+				if($_POST['tags']){
+						wp_set_post_tags($post_id, $_POST['tags']);
+						wp_set_post_tags($post_id+1, $_POST['tags']);
+					}
+				foreach($post_category as $p){
+					wp_set_post_categories( $post_id, $p,true );
+					wp_set_post_categories( $post_id+1, $p,true );
+				}
+				
+				$post   = get_post( $post_id );
+				
+				$image=upg_image_src('large',$post);
+				
+				do_action( "upg_submit_complete");
+				
+				if(isset($options['publish']) && $options['publish']=='1' )
+				{
+
+				echo "<h2>".__('Your Photo Has Been Added.','wp-upg')."</h2>";
+				echo "<a href='".esc_url( get_permalink($post_id) )."' class=\"pure-button\">".__('View Photo Here','wp-upg')."</a><br><br><hr>";
+				//echo "<h2>".__('Add Another Photo:')."</h2>";
+				
+				}
+			else
+			{
+				echo "<h2>".__('Your submission is under review.','wp-upg')."</h2>";
 				
 			}
-			
-			//Ended to submit extra fields
-			if($_POST['tags']){
-					wp_set_post_tags($post_id, $_POST['tags']);
-					wp_set_post_tags($post_id+1, $_POST['tags']);
+				
+				//echo "<h1 class=\"archive-title\">".$post->post_title."</h1>";
+				//echo "<img src='$image'>";
+			}
+			else
+			{
+				if ($error) 
+				{
+					$e = implode(',', $error);
+					$e = trim($e, ',');
+				} 
+				else 
+				{
+					$e = 'error';
 				}
-			wp_set_post_categories( $post_id, $post_category,true );
-			wp_set_post_categories( $post_id+1, $post_category,true );
-			
-			$post   = get_post( $post_id );
-			
-			$image=upg_image_src('large',$post);
-			
-			do_action( "upg_submit_complete");
-			
-			if(isset($options['publish']) && $options['publish']=='1' )
-			{
-
-			echo "<h2>".__('Your Photo Has Been Added.','wp-upg')."</h2>";
-			echo "<a href='".esc_url( get_permalink($post_id) )."' class=\"pure-button\">".__('View Photo Here','wp-upg')."</a><br><br><hr>";
-			echo "<h2>".__('Add Another Photo:')."</h2>";
-			
+				
+				echo "<h1>".__($e.' error','wp-upg')."</h1>";
 			}
-		else
-		{
-			echo "<h2>".__('Your submission is under review.','wp-upg')."</h2>";
-			
-		}
-			
-			//echo "<h1 class=\"archive-title\">".$post->post_title."</h1>";
-			//echo "<img src='$image'>";
-		}
-		else
-		{
-			if ($error) 
-			{
-				$e = implode(',', $error);
-				$e = trim($e, ',');
-			} 
-			else 
-			{
-				$e = 'error';
-			}
-			
-			echo "<h1>".__($e.' error','wp-upg')."</h1>";
-		} 
+		}		
 		
 	?>
 
